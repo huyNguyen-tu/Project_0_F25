8b6d7fcebc94063512ced9cbc332e382dcc58a31
huyNguyen <huy.nguyen1@temple.edu>
2025-08-27 13:44:38 -0400

    Create array holding possible letter to show up in the word

--- print_random_string.c
+++ print_random_string.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -16,19 +14,17 @@ char* rand_string_alloc(size_t size)
      return s;
 }
 
-
-
 int main()
 
 {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size =  10;  //specify a value for size of word
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
-    str = rand_string_alloc(size );
+    str = rand_string_alloc(size);
     printf("%s",str);
     free (str);
     
--- random.c
+++ random.c
@@ -2,11 +2,13 @@
 #include<string.h>
 
 
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size); // to be developed by the student
-     }
-     return s;
+char* rand_string(char* s, size_t size) {
+    char possibleLetter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    // for loop to assign a random letter to each index in array s up to its size
+    for ( int i = 0; i < size; i++) {
+        s[i] = possibleLetter[]; // pick a random letter in possibleLetter 
+    }
+    s[size] = '\0';
+    return s;
 }
